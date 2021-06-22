9611eb29a360fe37ed7e2dab2dbe5af6bd2f6a86
Cdawn628 <75674719+Cdawn628@users.noreply.github.com>
2021-06-22 21:04:53 +0800

    Update answers.js

---
+++ schema/answers.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+module.exports = new mongoose.Schema({
+    "qid":Number,//问卷号，获取所答问卷生成
+    "ans_qid" :Number,//答卷号，系统自动生成，唯一
+    //"ans_title" : String,//问卷标题
+    "ans_time" : String,//填写时间
+    "ans_list" : [{
+        //第一题
+        "ans_aid" : Number,//题号同aid
+        //"type" : Number,//题型，1 单选；2 多选；3 问答
+        "ans_isNecessary" : Boolean,//是否必填项
+       // "ask" : String,//题目
+        "ans" : String,//问答内容，可为空
+        "choice" : String, //所选选项，可为空
+       },{
+        //第二题
+    }]
+},{versionKey:false})
