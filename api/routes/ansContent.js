0b0d558b65a523c1c66266615fa7521bcd22f084
Cdawn628 <75674719+Cdawn628@users.noreply.github.com>
2021-07-06 22:23:00 +0800

    Update ansContent.js

--- api/routes/ansContent.js
+++ api/routes/ansContent.js
@@ -5,21 +5,44 @@ let quesModel=require('../../../model/questionnaires')
 function checkAnsContent(req,res)
 {
 var Qid =req.body.qid;
-
-new Promise((resolve, reject) => {
+var Ans_Qid =req.body.ans_qid;
+var out1={},out2={};
+let promise1=new Promise((resolve, reject) => {
     ansModel
-        .find({qid:Qid})
+        .find({ans_qid:Ans_Qid})
         .then(result => {
-            let responseData = {data:{} }
-          
-            responseData=result;
-            console.log(responseData)
-            util.responseClient(res, 200, 1, 'success', responseData)
-            
+           
+          out1=result;
+          resolve();
         })
     }).catch((err) => {
         console.log(err);
-}) 
-}
+})
 
+let promise2=new Promise((resolve, reject) => {
+    quesModel
+        .find({qid:Qid})
+        .then(result => {
+            out2=result;
+            resolve();
+        })
+    }).catch((err) => {
+        console.log(err);
+})
+
+Promise.all([promise1,promise2]).then(function()
+    {
+           let responseData = {data1:{},data2:{}}
+            responseData.data1=out1;
+            responseData.data2=out2;
+            console.log(responseData)
+            util.responseClient(res, 200, 1, 'success', responseData)
+            
+        
+    }).catch((err) => {
+        console.log(err);
+
+
+})
+}
 module.exports = checkAnsContent
