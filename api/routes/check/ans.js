57fad904ca7cbf3fa427eadd022faf9b743a818b
Cdawn628 <75674719+Cdawn628@users.noreply.github.com>
2021-07-08 18:34:33 +0800

    Update ans.js

--- api/routes/check/ans.js
+++ api/routes/check/ans.js
@@ -1,3 +1,14 @@
+/*url: '/api/check/ans' //查看答卷情况
+method: 'post',
+params: 
+    qid:'',
+
+res: {
+    ans_qid:1234,//答卷号
+    ans_time: '',//填写时间
+  
+}*/
+
 let util =require('../util')
 let ansModel=require('../../../model/answers')
 
@@ -9,12 +20,15 @@ new Promise((resolve, reject) => {
     ansModel
         .find({qid:Qid})
         .then(result => {
+            
             let responseData = {data:{} }
           
             responseData=result;
             console.log(responseData)
+            if(result[0])
             util.responseClient(res, 200, 1, 'success', responseData)
-            
+            else
+            util.responseClient(res, 200, 0, 'not exists', responseData)
         })
     }).catch((err) => {
         console.log(err);
