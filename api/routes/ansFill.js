1b5ee385b8e70c0befcb7a899cdddf1f99952e9c
Cdawn628 <75674719+Cdawn628@users.noreply.github.com>
2021-07-16 00:32:24 +0800

    Update ansFill.js

--- api/routes/ansFill.js
+++ api/routes/ansFill.js
@@ -23,65 +23,111 @@ let util = require('./util')
 let quesModel = require('../../model/questionnaires')
 let ansModel=require('../../model/answers');
 const { callbackify } = require('util');
+
 function ansFill(req, res){
     const form = {};
+    const state={};
+    //state.flag=1;
     if(req.body.username) form.username = req.body.username;
     if(req.body.qid) form.qid = req.body.qid;
     //console.log(1);
     if(req.body.ans_list) form.ans_list=req.body.ans_list;
+     var t=new Date().getTime();
  // var t= Date.now();
   //t.toLocaleString( );
-  // console.log(t);
+   console.log(t);
    form.ans_time=req.body.ans_time;
- let promise1= new Promise((resolve, reject) => {
 
-        ansModel  
-        .find({}).sort({ans_qid: -1})
-        .then(result => {
-            console.log(result[0]);
-            form.ans_qid = result[0].ans_qid + 1;
-            resolve(form);
-        })
+
+
+quesModel.find({qid:form.qid}).then(result=>
+    {//var t0,status; console.log(1)
+        state.t0=result[0].time;
+        state.status=result[0].status;
+        //state.console.log(t0);
+        console.log(state)
+        if(t<state.t0) 
+        {
+        let promise1= new Promise((resolve, reject) => {
+        
+            //console.log(state)
            
-        }).then(form=>
-            {
-                
-               // console.log(total);
-                //插入答卷表数据
-               ansModel.create(form).then((doc) => 
-                {
-                    let responseData = { data1: {},data2:{} }
-                    responseData.data2 = doc;
-                    console.log(responseData)
-                    util.responseClient(res, 200, 1, 'success', responseData)
+                ansModel  
+                .find({}).sort({ans_qid: -1})
+                .then(result => {
+                    console.log(result[0]);
+                    form.ans_qid = result[0].ans_qid + 1;
+                    resolve(form);
                 })
-
-               
+            
+                }).then(form=>
+                    {
+                        if(state.status==2)
+                        {
+                            let responseData = { data1: {},data2:{} }
+                            util.responseClient(res, 200, 2, '问卷已过期', responseData)
+                        }
+                        else
+                       // console.log(total);
+                        //插入答卷表数据
+                       ansModel.create(form).then((doc) => 
+                        {
+                            let responseData = { data1: {},data2:{} }
+                            responseData.data2 = doc;
+                            console.log(responseData)
+                            util.responseClient(res, 200, 1, 'success', responseData)
+                        })
+        
+                       
+                        
+                    }).catch((err) => {
+                        console.log(err);
+                    
+                    });
+                    
                 
-            }).catch((err) => {
-                console.log(err);
+        
+        
+        
+        
+        let promise2=new Promise((resolve,reject)=>
+        {
+             //更新问卷表次数+1
+                      
+             if(state.status==2)
+             {
+                 let responseData = { data1: {},data2:{} }
+                 util.responseClient(res, 200, 2, '问卷已过期', responseData)
+             }
+             else
+                        quesModel.findOneAndUpdate(
+                            { qid: form.qid },
+                            { $inc:{ total: +1 } }).then((doc)=> {
+                               // let responseData = { data1: {},data2:{} }
+                                //responseData.data1 = doc;
+                                //console.log(responseData)
+                                //util.responseClient(res, 200, 1, 'success', responseData)
+                            })
+                        
+        });
+        
+        Promise.all([promise1,promise2]).then(function(){
+        
             
-            
-    
-    
-            });
+        });
+        }
+        else
+        {
+            quesModel.findOneAndUpdate(
+                { qid: form.qid },
+                { $set:{ status: 2 } }).then((doc)=> {
+                   let responseData = { data1: {},data2:{} }
+                    //responseData.data1 = doc;
+                    //console.log(responseData)
+                    util.responseClient(res, 200, 2, '过期', responseData)
+                })
+        }
+})
 
-let promise2=new Promise((resolve,reject)=>
-{
-     //更新问卷表次数+1
-               
-                  
-                quesModel.findOneAndUpdate(
-                    { qid: form.qid },
-                    { $inc:{ total: +1 } }).then((doc)=> {
-                       // let responseData = { data1: {},data2:{} }
-                        //responseData.data1 = doc;
-                        //console.log(responseData)
-                        //util.responseClient(res, 200, 1, 'success', responseData)
-                    })
-
-});
-
-Promise.all([promise1,promise2]).then(function(){callbackify(null,data);});
 }
 module.exports = ansFill
